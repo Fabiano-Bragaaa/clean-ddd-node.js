4df1a2588f2ec9085cfc6de34ca8f3cf48b25250
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-25 21:17:14 -0300

    Getters & Setters das entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,7 @@ import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../types/optional"
+import dayjs from 'dayjs'
 
 interface QuestionProps {
   authorId :UniqueEntityId
@@ -15,9 +16,71 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps>{
-  static create(props:Optional<QuestionProps, 'createdAt'> , id?: UniqueEntityId){
+
+   get authorId() {
+    return this.props.authorId
+  }
+
+   get slug() {
+    return this.props.slug
+  }
+  
+  get title() {
+    return this.props.title
+  }
+
+   get content() {
+    return this.props.content
+  }
+
+   get createdAt() {
+    return this.props.createdAt
+  }
+
+   get updatedAt() {
+    return this.props.updatedAt
+  }
+
+   get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  
+   get isNew():boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+    get excerpt() {
+    return this.content.substring(0, 120).trim().concat('...')
+  }
+ 
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+
+    set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  
+
+  static create(props:Optional<QuestionProps, 'createdAt' | 'slug'> , id?: UniqueEntityId){
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: new Date()
     }, id)
 
@@ -25,4 +88,3 @@ export class Question extends Entity<QuestionProps>{
 
   }
 }
-
