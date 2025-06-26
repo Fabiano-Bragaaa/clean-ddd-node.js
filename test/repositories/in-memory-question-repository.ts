a690425784ec4199633aabba90af6d3ddd488ebb
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-26 20:43:18 -0300

    Caso de uso: Deletar resposta

--- test/repositories/in-memory-question-repository.ts
+++ test/repositories/in-memory-question-repository.ts
@@ -2,6 +2,26 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionRepository implements QuestionsRepository {
+  public items: Question[] = []
+
+  async findById(questionId: string) {
+    const question = this.items.find(
+      (item) => item.id.toString() === questionId,
+    )
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async delete(question: Question) {
+    const itemIdex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIdex, 1)
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
 
@@ -11,8 +31,6 @@ export class InMemoryQuestionRepository implements QuestionsRepository {
 
     return question
   }
-
-  public items: Question[] = []
 
   async create(question: Question) {
     this.items.push(question)
