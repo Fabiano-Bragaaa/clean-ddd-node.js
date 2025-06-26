2e0c35e0fc58cfb465167fb56304565b7f848770
Fabiano Braga <fabianobragadev@gmail.com>
2025-06-26 20:14:19 -0300

    Factories de testes

--- src/domain/forum/application/use-cases/__tests__/get-question-by-slug.test.ts
+++ src/domain/forum/application/use-cases/__tests__/get-question-by-slug.test.ts
@@ -1,7 +1,6 @@
 import { InMemoryQuestionRepository } from 'test/repositories/in-memory-question-repository'
 import { GetQuestionBySlugUseCase } from '../get-question-by-slug'
-import { Question } from '@/domain/forum/enterprise/entities/question'
-import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
 
 let inMemoryQuestionRepository: InMemoryQuestionRepository
 let sut: GetQuestionBySlugUseCase
@@ -11,11 +10,7 @@ describe('Get Question by Slug Use Case', () => {
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionRepository)
   })
   it('should be able to get a question with slug param', async () => {
-    const newQuestion = Question.create({
-      title: 'Example question',
-      authorId: new UniqueEntityId('1'),
-      content: 'some content',
-    })
+    const newQuestion = makeQuestion()
 
     await inMemoryQuestionRepository.create(newQuestion)
 
--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -4,7 +4,7 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/types/optional'
 import dayjs from 'dayjs'
 
-interface QuestionProps {
+export interface QuestionProps {
   authorId: UniqueEntityId
   slug: Slug
   bestAnswerId?: UniqueEntityId
